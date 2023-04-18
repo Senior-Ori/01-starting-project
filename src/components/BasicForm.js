2ed5dfe7e69408b3a7d7e76718e291494acce438
senior_Ori <ori8834@gmail.com>
2023-04-18 14:50:57 +0300

    idk/

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,37 @@
+import userInput from "../hooks/user-input";
 const BasicForm = (props) => {
+  const {
+    value: enteredName,
+    hasError: nameHasError,
+    isTouched: nameIsTouched,
+    isValid: nameIsValid,
+    resetValue: resetName,
+  } = userInput((value) => value.trim() !== "");
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input
+            onBlur={nameIsTouched}
+            value={enteredName}
+            type="text"
+            id="name"
+          />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="email" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
